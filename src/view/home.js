da9fa32e60a892171b076fcf2d0649fee533cb6d
meryvera <yrem.vera.5@gmail.com>
2021-04-22 16:58:43 -0500

    Fixing css breakpoints

--- src/view/home.js
+++ src/view/home.js
@@ -1,13 +1,14 @@
 const viewHome = () => {
-  const view = ` <div class="wraper">
-  <section class="container container-home">
-    <h1 class="container-home__h1">Travelers</h1>
-    <h3 class="container-home__h3 align-start">Join the adventure!</h3>
-    <button id="login" class="button">Log In</button>
-    <p class="container-home__p">------- or -------</p>
-    <button id="signUp" class="button button--white">Sign Up</button>
-  </section>
-</div> `;
+  const view = ` 
+  <div class="wraper">
+    <section class="container container-home">
+      <h1 class="container-home__h1">Travelers</h1>
+      <h3 class="container-home__h3 align-start">Join the adventure!</h3>
+      <button id="login" class="button">Log In</button>
+      <p class="container-home__p">------- or -------</p>
+      <button id="signUp" class="button button--white">Sign Up</button>
+    </section>
+  </div> `;
 
   const articleElem = document.createElement('article');
   articleElem.innerHTML = view;
