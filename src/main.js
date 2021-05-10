7ba627739a05c862eebd2c3c66f51fbc28f530b9
thainole <thainole@gmail.com>
2021-05-10 15:23:16 -0500

    fix bugs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { changeView } from './router.js';
 
 const init = () => {
-  console.log('iniciando');
   if (window.location.hash === '') {
     window.location.hash = '#/';
-    console.log('hash');
   }
   changeView(window.location.hash);
   window.addEventListener('hashchange', () => changeView(window.location.hash));
